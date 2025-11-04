8c410e962c031e503cc77cbf4c09de1c6ae3481a
Sandeep <thesandeep2080@gmail.com>
2025-11-04 20:14:08 +0545

    Responsive

--- src/components/Layout/HeroSection/HeroSection.jsx
+++ src/components/Layout/HeroSection/HeroSection.jsx
@@ -1,7 +1,7 @@
 // src/components/Hero.jsx
 import React from "react";
 import { Link } from "react-router-dom";
-import "./HeroSection.css"
+import "./HeroSection.css";
 
 const Hero = () => {
   return (
@@ -11,10 +11,14 @@ const Hero = () => {
           <h1>Land the Perfect Job for Your Skills and Interests</h1>
           <p>
             Connect with exciting IT, creative, and multimedia projects, and
-            bring your ideas to life with modern tools and innovative approaches.
+            bring your ideas to life with modern tools and innovative
+            approaches.
           </p>
           <div className="hero-buttons">
-            <Link to="/under_construction" className="hero-action-btn post-job-btn">
+            <Link
+              to="/under_construction"
+              className="hero-action-btn post-job-btn"
+            >
               Post a Job
             </Link>
             <Link to="/findjob" className="hero-action-btn apply-job-btn">
@@ -23,7 +27,10 @@ const Hero = () => {
           </div>
         </div>
         <div className="hero-image">
-          <img src="/icons/visual-collaboration.svg" alt="Visual Collaboration Illustration" />
+          <img
+            src="/icons/visual-collaboration.svg"
+            alt="Visual Collaboration Illustration"
+          />
         </div>
       </div>
     </section>
